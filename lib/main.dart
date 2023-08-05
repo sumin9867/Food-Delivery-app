2496d9711bc54c6d504865d10ab174788406d4c0
Rabin GC <rabingc05@gmail.com>
2023-08-05 14:18:06 +0545

    new update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:project2/pages/food/recomdedfooddetail.dart';
+import 'package:project2/pages/home/main_FoodPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +13,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        debugShowCheckedModeBanner: false, home: RecomendedFoodDetail());
+        debugShowCheckedModeBanner: false, home: MainFoodPage());
   }
 }
